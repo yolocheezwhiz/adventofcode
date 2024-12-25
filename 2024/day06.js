747ba7fbd691c7e29fdc03d7741925d37dc673bd
MO <98034178+yolocheezwhiz@users.noreply.github.com>
2024-12-25 12:12:22 -0500

    Update day06.js

--- 2024/day06.js
+++ 2024/day06.js
@@ -6,6 +6,8 @@ const startTime = Date.now();
 
 // Day-specific setup
 const map = localStorage[day].split('\n').map(line => line.split(''));
+const mapCopy = JSON.stringify(map.map(line => [...line]));
+const mapLen = map.length;
 const moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 const answerp1 = new Set();
 const pos = {};
@@ -34,8 +36,8 @@ function walk(p2Set, pos, dir, map) {
     // Loop until the guard exists the map, or gets stuck in an endless loop
     while (true) {
         // Keep track of both the position, and from which direction its been visited
-        posKey = `${pos.y}_${pos.x}`;
-        posDirKey = `${posKey}_${dir}`;
+        posKey = pos.y + pos.x * mapLen
+        posDirKey = posKey + dir * (mapLen ** 2);
         // Part 1 
         // We use the existence of p2Set as an indicator of whether we're in the recursive function or the main one
         // If in main loop add the position to part 1 answer
@@ -53,16 +55,16 @@ function walk(p2Set, pos, dir, map) {
         nextPos.y = pos.y + moves[dir][0];
         nextPos.x = pos.x + moves[dir][1];
         // If out of bounds, exit loop
-        if (nextPos.y < 0 || nextPos.x < 0 || nextPos.y >= map.length || nextPos.x >= map[0].length) break;
+        if (nextPos.y < 0 || nextPos.x < 0 || nextPos.y >= mapLen || nextPos.x >= mapLen) break;
         // Verify it's not an obstacle
         else if (map[nextPos.y][nextPos.x] !== '#') {
             // If Part 1 and the next position has not already been visited
             // (incidentally prevents trying to put an obstacle at the guard's original position)
-            if (!p2Set && !answerp1.has(`${nextPos.y}_${nextPos.x}`)) {
+            if (!p2Set && !answerp1.has(nextPos.y + nextPos.x * mapLen)) {
                 // Clone map, change the next pos for an obstacle, recurse for part 2
-                const mapCopy = map.map(line => [...line]);
-                mapCopy[nextPos.y][nextPos.x] = '#';
-                walk(new Set(posDirKey), { ...pos }, dir, mapCopy);
+                const copy = JSON.parse(mapCopy);
+                copy[nextPos.y][nextPos.x] = '#';
+                walk(new Set(), { ...pos }, dir, copy);
             }
             // Set pos to next pos.
             pos.y = nextPos.y;
